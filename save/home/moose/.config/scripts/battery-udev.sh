bb9013696a0b7feaa8f7c6e2cc4ff4bc7004ff43
Ido Ilouz <ilouzido@gmail.com>
2025-04-07 13:06:46 +0300

    tmp

--- save/home/moose/.config/scripts/battery-udev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export DISPLAY=:0
-export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
-
-STATUS=`cat /sys/class/power_supply/BAT0/status`
-CAPACITY=`cat /sys/class/power_supply/BAT0/capacity`
-
-su moose -c "notify-send $STATUS 'Capacity: $CAPACITY%'"
