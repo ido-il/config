1a60e7ea2bd1d073a4ca0ebd0865856b33ba673b
Ido Ilouz <ilouzido@gmail.com>
2025-04-07 13:06:54 +0300

    Backup on 2025-04-07_13:06:53  +0300

---
+++ save/home/moose/.config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,37 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      enable_git_status = true,
+      enable_diagnostics = true,
+
+      event_handlers = {
+      {
+        event = "file_opened",
+        handler = function(file_path)
+          require("neo-tree.command").execute({ action = "close" })
+        end
+      },
+    },
+
+      window = {
+        position = "right"
+      },
+
+      filesystem = {
+       visible = true,
+       enabled = false,
+       hijack_netrw_behavior = "open_default",
+      }
+    })
+
+    vim.keymap.set('n', '<C-e>', ':Neotree filesystem focus right<CR>', {})
+  end
+}
