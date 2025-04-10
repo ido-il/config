3886aa5dc42e2a49be91403a6d308f548dc25443
Ido Ilouz <ilouzido@gmail.com>
2025-04-10 14:44:08 +0300

    add save prompt + git staged to show all diffs

--- save.sh
+++ save.sh
@@ -14,23 +14,30 @@ mkdir -p $SAVE_DIR
 
 # copy indexed files to save dir
 for dest in $(cat $INDEX_FILE); do
-  if [ -f $dest ] || [ -d $dest ]; then
-    mkdir -p "$(pwd)/$SAVE_DIR$(dirname $dest)"
-    cp -r $dest "$(pwd)/$SAVE_DIR$dest"
-  fi
+    if [ -f $dest ] || [ -d $dest ]; then
+        mkdir -p "$(pwd)/$SAVE_DIR$(dirname $dest)"
+        cp -r $dest "$(pwd)/$SAVE_DIR$dest"
+    fi
 done
 
 # remove submodules by force
 find "$(pwd)/save" -type d -name ".git" -exec rm -rf {} +
 
-# show changes
-git diff
+git add save/
+git diff --staged
+git status
 
-# initiate ssh agent
-eval $(ssh-agent -s)
-ssh-add $GIT_KEY
+read -p "Would you like to save these chanegs? [y/N]: " answer
 
-# save to repo
-git add .
-git commit -m "Backup on $DATE"
-git push
+if [[ "$answer" = "y" || "$answer" = "Y" ]]; then
+    # initiate ssh agent
+    # TODO: better method to push with SSH without initiating a new agent every time
+    eval $(ssh-agent -s)
+    ssh-add $GIT_KEY
+    
+    # save to repo
+    git commit -m "Backup on $DATE"
+    git push
+else
+    git restore --staged save/
+fi
