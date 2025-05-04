bb09ac54bd14bbcc7f797ce23e78467476274396
Ido Ilouz <ilouzido@gmail.com>
2025-05-04 20:49:32 +0300

    Backup on 2025-05-04_20:49:04  +0300

--- save/home/moose/.config/scripts/init.sh
+++ save/home/moose/.config/scripts/init.sh
@@ -4,9 +4,6 @@
 ~/.fehbg
 
 # daemon apps
-nm-applet &
-blueman-applet &
 sxhkd &
 dunst &
 picom &
-polybar -c $XDG_CONFIG_HOME/polybar/config.ini &
--- save/home/moose/.config/st/config.h
+++ save/home/moose/.config/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "BigBlueTermPlus Nerd Font Mono:size=8";
+static char *font = "BigBlueTermPlus Nerd Font Mono:size=10";
 static int borderpx = 2;
 
 /*
