e2b9445045c263f6ed37aa8a90d069683a96ce52
Ido Ilouz <ilouzido@gmail.com>
2025-05-22 21:12:54 +0300

    add exclusion via `!`

--- save.sh
+++ save.sh
@@ -2,25 +2,30 @@
 
 set -e 
 
-# variables
 INDEX_FILE="index"
-SAVE_DIR="save"
-DATE=$(date +"%Y-%m-%d_%H:%M:%S  %z") 
 GIT_KEY="$HOME/.ssh/git"
+DATE=$(date +"%Y-%m-%d_%H:%M:%S  %z") 
+SAVE_DIR="$PWD/save"
 
-# clear current save dir
-rm -rf $SAVE_DIR
-mkdir -p $SAVE_DIR
+rm -rf "$SAVE_DIR"
+mkdir -p "$SAVE_DIR"
 
-# copy indexed files to save dir
-for dest in $(cat $INDEX_FILE); do
-    if [ -f $dest ] || [ -d $dest ]; then
-        mkdir -p "$(pwd)/$SAVE_DIR$(dirname $dest)"
-        cp -r $dest "$(pwd)/$SAVE_DIR$dest"
-    fi
+for path in $(cat $INDEX_FILE | grep "^/*"); do
+	echo $path
+	[[ ! -e "$path" ]] && continue
+	save_path="$SAVE_DIR$path"
+	mkdir -p "$(dirname $save_path)"
+    cp -r "$path" "$save_path"
 done
 
-# remove submodules by force
+for path in $(cat $INDEX_FILE | grep "^!/*"); do
+	path=$(echo "$path" | sed 's/!//')
+	[[ ! -e "$path" ]] && continue
+	save_path="$SAVE_DIR$path"
+	mkdir -p "$(dirname $save_path)"
+    rm -rf "$save_path"
+done
+
 find "$(pwd)/save" -type d -name ".git" -exec rm -rf {} +
 
 git add save/
@@ -29,13 +34,7 @@ git status
 
 read -p "Would you like to save these chanegs? [y/N]: " answer
 
-if [[ "$answer" = "y" || "$answer" = "Y" ]]; then
-    # initiate ssh agent
-    # TODO: better method to push with SSH without initiating a new agent every time
-    eval $(ssh-agent -s)
-    ssh-add $GIT_KEY
-    
-    # save to repo
+if [[ "$answer" = "y" || "$answer" = "Y" ]]; then    
     git commit -m "Backup on $DATE"
     git push
 else
