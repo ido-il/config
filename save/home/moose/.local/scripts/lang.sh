f49767bb787cea29af8ec77cbcafb93bb425672a
Ido Ilouz <ilouzido@gmail.com>
2025-06-08 12:10:45 +0300

    Backup on 2025-06-08_12:10:30  +0300

--- save/home/moose/.local/scripts/lang.sh
+++ save/home/moose/.local/scripts/lang.sh
@@ -1,25 +1,40 @@
 #!/bin/sh
 
-change_lang ()
-{
-  echo $1 > /tmp/lang
-  setxkbmap $1
-  notify-send -u low "Changed language" $2
-}
+LANGS=`cat <<EOF
+us:English
+il:Hebrew
+EOF`
 
-fallback () {
-  echo "us" > /tmp/lang
-}
+CURRENT=$(setxkbmap -query | awk '/layout:/ {print $2}')
 
-# prevent issues if file wasnt created/deleted
-[ ! -f /tmp/lang ] && fallback
+NEXT_KEYMAP=""
+NEXT_LABEL=""
+FOUND=0
 
-# get current keymap
-lang="$(cat /tmp/lang)"
+while IFS= read -r line; do
+    KEYMAP=$(echo "$line" | cut -d: -f1)
+    LABEL=$(echo "$line" | cut -d: -f2-)
 
-# toggle language
-case $lang in
-  "us") change_lang il "Hebrew";;
-  "il") change_lang us "English";;
-  *) fallback && notify-send "lang error" "how?..."
-esac
+    if [ "$FOUND" = 1 ]; then
+        NEXT_KEYMAP=$KEYMAP
+        NEXT_LABEL=$LABEL
+        break
+    fi
+
+    [ "$KEYMAP" = "$CURRENT" ] && FOUND=1
+
+    [ -z "$FIRST_KEYMAP" ] && FIRST_KEYMAP=$KEYMAP && FIRST_LABEL=$LABEL
+done <<EOF
+$LANGS
+EOF
+
+if [ "$FOUND" = 0 ]; then
+	notify-send -u critical "Error: unrecognize language" "current keymap '$CURRENT'"
+	exit 1
+elif [ -n "$NEXT_KEYMAP" ]; then
+    setxkbmap "$NEXT_KEYMAP"
+    notify-send -u low "Switch language: $NEXT_LABEL"
+else
+    setxkbmap "$FIRST_KEYMAP"
+    notify-send -u low "Switch language: $FIRST_LABEL"
+fi
